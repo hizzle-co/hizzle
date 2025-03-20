fc04bec7f699a8d5eab4d27007314257bb441dba
picocodes <picocodes@gmail.com>
2025-03-20 15:20:32 +0300

    chagnes

--- .eslintrc.js
+++ .eslintrc.js
@@ -14,6 +14,7 @@ module.exports = {
 		eqeqeq: 'warn',
 		'no-console': 'warn',
 		'@wordpress/no-unused-vars-before-return': 'off',
+		'@wordpress/no-unsafe-wp-apis': 'off',
 		'react/react-in-jsx-scope': 'off',
 		'@typescript-eslint/explicit-module-boundary-types': 'off',
 		'@typescript-eslint/no-explicit-any': 'off',
--- packages/components/src/components/utils/get-merge-tag-value.ts
+++ packages/components/src/components/utils/get-merge-tag-value.ts
@@ -6,15 +6,17 @@ import type { smartTag } from '../hooks';
 /**
  * Returns a merge tag's value.
  *
+ * @param {smartTag} smartTag - The smart tag to get the value of.
+ * @returns {string} The value of the smart tag.
  */
-export const getMergeTagValue = (smartTag: smartTag) => {
-	if (smartTag.example) {
+export const getMergeTagValue = ( smartTag: smartTag ) : string => {
+	if ( smartTag.example ) {
 		return smartTag.example;
 	}
 
-	if (!smartTag.default) {
-		return `${smartTag.smart_tag}`;
+	if ( !smartTag.default ) {
+		return `${ smartTag.smart_tag }`;
 	}
 
-	return `${smartTag.smart_tag} default="${smartTag.default}"`;
+	return `${ smartTag.smart_tag } default="${ smartTag.default }"`;
 };
