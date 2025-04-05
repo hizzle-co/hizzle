61023121e7cf8793dbe10a0cccdaf4ab6931ae4e
picocodes <picocodes@gmail.com>
2025-04-05 12:26:02 +0300

    feat: add bulk actions support and improve table context management

--- packages/records/src/components/table/context.tsx
+++ packages/records/src/components/table/context.tsx
@@ -6,7 +6,6 @@ import React, {
 	useContext,
 	ReactNode,
 	useMemo,
-	useState,
 } from 'react';
 
 import {
@@ -15,9 +14,14 @@ import {
 	getFilteredRowModel,
 	TableOptions,
 	Table,
-	ColumnOrderState,
 	TableState,
 } from '@tanstack/react-table';
+
+
+/**
+ * WordPress dependencies
+ */
+import { CheckboxControl } from '@wordpress/components';
 
 /**
  * Interface for the TableContext
@@ -62,7 +66,14 @@ export interface TableProviderProps<TData>
 	/**
 	 * On change of the table state
 	 */
-	onChange?: (state: Partial<TableState>) => void;
+	onChange?: ( state: Partial<TableState> ) => void;
+
+	/**
+	 * Optional function to render bulk actions UI.
+	 * @param selected - The currently selected items.
+	 * @returns React node to render for bulk actions.
+	 */
+	bulkActions?: ( selected: string[], isAllSelected: boolean, isAllPagesSelected: boolean ) => React.ReactNode;
 }
 
 /**
@@ -75,68 +86,115 @@ const TableContext = createContext<TableContextProps<any> | undefined>(
 /**
  * Provider component for the table context
  */
-export function TableProvider<TData>({
+export function TableProvider<TData>( {
 	children,
 	enableSorting = true,
 	enableFiltering = true,
 	enablePagination = true,
 	onChange,
 	state = {},
+	columns,
+	bulkActions,
 	...tableOptions
-}: TableProviderProps<TData>) {
-	const table = useReactTable({
+}: TableProviderProps<TData> ) {
+
+	const tableColumns = useMemo( () => {
+		if ( !bulkActions ) {
+			return columns;
+		}
+
+		return [
+			{
+				id: 'hizzlewp-selection',
+				header: ( { table } ) => {
+					const state = table.getState();
+					console.log( state );
+					return (
+						<CheckboxControl
+							type="checkbox"
+							checked={ table.getIsAllPageRowsSelected() }
+							onChange={ table.toggleAllPageRowsSelected }
+							indeterminate={ table.getIsSomeRowsSelected() }
+							aria-label={ table.getIsAllPageRowsSelected() ? 'Deselect all' : 'Select all' }
+							className="hizzle-records__table-selection-checkbox"
+							__nextHasNoMarginBottom
+						/>
+					)
+				},
+				cell: ( { row } ) => (
+					<CheckboxControl
+						type="checkbox"
+						checked={ row.getIsSelected() }
+						onChange={ row.toggleSelected }
+						disabled={ !row.getCanSelect() }
+						aria-label={ row.getIsSelected() ? 'Unselect item' : 'Select item' }
+						className="hizzle-records__table-selection-checkbox"
+						data-row={ row }
+						__nextHasNoMarginBottom
+					/>
+				),
+				enableSorting: false,
+				enableHiding: false,
+			},
+			...columns,
+		];
+	}, [ columns, bulkActions ] );
+
+	const table = useReactTable( {
 		...tableOptions,
+		columns: tableColumns,
 		getCoreRowModel: getCoreRowModel(),
 		state,
 		enableSorting,
-		onColumnOrderChange: (columnOrder) => {
-			if (onChange && columnOrder) {
-				onChange({ ...state, columnOrder });
+		enableRowSelection: true,
+		onColumnOrderChange: ( columnOrder ) => {
+			if ( onChange && columnOrder ) {
+				onChange( { ...state, columnOrder } );
 			}
 		},
-		onColumnVisibilityChange: (columnVisibility) => {
-			if (onChange && columnVisibility) {
-				onChange({
+		onColumnVisibilityChange: ( columnVisibility ) => {
+			if ( onChange && columnVisibility ) {
+				onChange( {
 					...state,
 					columnVisibility: columnVisibility(
 						state.columnVisibility || {}
 					),
-				});
+				} );
 			}
 		},
-		...(enableSorting && {
+		...( enableSorting && {
 			manualSorting: true,
-			onSortingChange: (sorting) => {
-				if (onChange && sorting) {
-					onChange({
+			onSortingChange: ( sorting ) => {
+				if ( onChange && sorting ) {
+					onChange( {
 						...state,
-						sorting: sorting(state.sorting),
+						sorting: sorting( state.sorting ),
 						pagination: {
 							pageSize: state.pagination?.pageSize || 10,
 							pageIndex: 0,
 						},
-					});
+					} );
 				}
 			},
-		}),
-		...(enableFiltering && { getFilteredRowModel: getFilteredRowModel() }),
-		...(enablePagination && {
+		} ),
+		...( enableFiltering && { getFilteredRowModel: getFilteredRowModel() } ),
+		...( enablePagination && {
 			manualPagination: true,
-			onPaginationChange: (pagination) => {
-				if (onChange && pagination) {
-					onChange({
+			onPaginationChange: ( pagination ) => {
+				if ( onChange && pagination ) {
+					onChange( {
 						...state,
-						pagination: pagination(state.pagination),
-					});
+						pagination: pagination( state.pagination ),
+					} );
 				}
 			},
-		}),
-	});
+		} ),
+	} );
 
-	const value = useMemo(() => ({ table }), [table]);
+	const value = useMemo( () => ( { table } ), [ table ] );
 
 	return (
-		<TableContext.Provider value={value}>{children}</TableContext.Provider>
+		<TableContext.Provider value={ value }>{ children }</TableContext.Provider>
 	);
 }
 
@@ -144,10 +202,10 @@ export function TableProvider<TData>({
  * Hook to use the table context
  */
 export function useTable<TData>() {
-	const context = useContext(TableContext);
+	const context = useContext( TableContext );
 
-	if (context === undefined) {
-		throw new Error('useTable must be used within a TableProvider');
+	if ( context === undefined ) {
+		throw new Error( 'useTable must be used within a TableProvider' );
 	}
 
 	return context.table as Table<TData>;
