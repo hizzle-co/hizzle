b2b2ab543f520fe0e91e86ac5ddab249d5a8cf7d
picocodes <picocodes@gmail.com>
2025-04-16 14:19:58 +0300

    reset filters button

---
+++ packages/records/src/components/filters/reset-filters.tsx
@@ -0,0 +1,30 @@
+/**
+ * WordPress dependencies
+ */
+import { Button } from '@wordpress/components';
+import { __ } from '@wordpress/i18n';
+
+/**
+ * Internal dependencies
+ */
+import { useTable } from '..';
+
+export const ResetFilter: React.FC = () => {
+    const table = useTable();
+    const filters = table.getState().columnFilters;
+
+    return (
+        <Button
+            disabled={ !Array.isArray( filters ) || filters.length === 0 }
+            accessibleWhenDisabled
+            size="compact"
+            variant="tertiary"
+            className="hizzlewp-records-filters__reset-button"
+            onClick={ () => {
+                table.resetColumnFilters();
+            } }
+        >
+            { __( 'Reset' ) }
+        </Button>
+    );
+}
