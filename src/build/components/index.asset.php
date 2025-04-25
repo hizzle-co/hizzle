88274b973ac7f908bdf3568b02d84dbd1a154f62
picocodes <picocodes@gmail.com>
2025-04-25 14:54:49 +0300

    fix: Includes conditions inverted

--- src/build/components/index.asset.php
+++ src/build/components/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-api-fetch', 'wp-components', 'wp-compose', 'wp-date', 'wp-element', 'wp-i18n', 'wp-keycodes', 'wp-primitives', 'wp-url'), 'version' => 'e623df4dce84160f9a84');
+<?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-api-fetch', 'wp-components', 'wp-compose', 'wp-date', 'wp-element', 'wp-i18n', 'wp-keycodes', 'wp-primitives', 'wp-url'), 'version' => '7a049a52b919693ebc6f');
