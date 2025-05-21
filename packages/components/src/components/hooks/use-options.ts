c2c5c941f91140ff14455db0405f0fdcd0071b95
picocodes <picocodes@gmail.com>
2025-05-21 08:34:16 +0300

    allow dynamic options

--- packages/components/src/components/hooks/use-options.ts
+++ packages/components/src/components/hooks/use-options.ts
@@ -8,6 +8,37 @@ import { useMemo } from 'react';
  */
 import { SelectOption } from '../select';
 
+const prepareOptions = ( options: string[] | SelectOption[] | Record<string, string> ) => {
+
+	// Arrays.
+	if ( Array.isArray( options ) ) {
+		// Check if the first option is an object (has label and value properties)
+		if (
+			options.length > 0 &&
+			typeof options[ 0 ] === 'object' &&
+			'label' in options[ 0 ] &&
+			'value' in options[ 0 ]
+		) {
+			return options as SelectOption[];
+		}
+
+		return options.map( ( label ) => {
+			return {
+				label,
+				value: label,
+			};
+		} );
+	}
+
+	// Objects.
+	return Object.keys( options ).map( ( value ) => {
+		return {
+			label: options[ value ],
+			value,
+		};
+	} );
+}
+
 /**
  * Prepares the available options for the selected condition.
  *
@@ -16,37 +47,21 @@ import { SelectOption } from '../select';
 export const useOptions = (
 	options: string[] | SelectOption[] | Record<string, string>
 ): SelectOption[] => {
-	return useMemo(() => {
-		if (!options) {
+	return useMemo( () => {
+		if ( !options ) {
 			return [];
 		}
 
-		// Arrays.
-		if (Array.isArray(options)) {
-			// Check if the first option is an object (has label and value properties)
-			if (
-				options.length > 0 &&
-				typeof options[0] === 'object' &&
-				'label' in options[0] &&
-				'value' in options[0]
-			) {
-				return options as SelectOption[];
+		// Dynamic options.
+		if ( 'string' === typeof options ) {
+			if ( window.hizzleWPComponentsSelectOptions?.[ options ] ) {
+				return prepareOptions( window.hizzleWPComponentsSelectOptions[ options ] );
 			}
 
-			return options.map((label) => {
-				return {
-					label,
-					value: label,
-				};
-			});
+			// If the option is not found, return an empty array.
+			return [];
 		}
 
-		// Objects.
-		return Object.keys(options).map((value) => {
-			return {
-				label: options[value],
-				value,
-			};
-		});
-	}, [options]);
+		return prepareOptions( options );
+	}, [ options ] );
 };
