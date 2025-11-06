54ea50ea44c78f43f1b3e9babdfc4904e146bb2c
picocodes <picocodes@gmail.com>
2025-11-06 11:37:17 +0300

    fix: Contains and Not Contains does not condition type not shown when there are options

--- packages/components/src/components/conditional-logic/rule.tsx
+++ packages/components/src/components/conditional-logic/rule.tsx
@@ -24,6 +24,8 @@ import {
 	smartTag as SmartTagType,
 } from '../hooks';
 
+const NON_OPTION_CONDITIONS = [ 'is_empty', 'is_between', 'is_not_empty', 'begins_with', 'ends_with' ];
+
 /**
  * A single conditional logic rule.
  */
@@ -56,8 +58,8 @@ type ConditionalLogicRuleProps = {
 	availableSmartTags: Record<string, SmartTagType>;
 	mergeTagsArray: SmartTagType[];
 	index: number;
-	updateRule: (index: number, value: Partial<ConditionalLogicRule>) => void;
-	removeRule: (index: number) => void;
+	updateRule: ( index: number, value: Partial<ConditionalLogicRule> ) => void;
+	removeRule: ( index: number ) => void;
 };
 
 /**
@@ -82,8 +84,8 @@ export const ConditionalLogicRule: React.FC<ConditionalLogicRuleProps> = (
 	 * @param value The value to update.
 	 */
 	const updateValue = useCallback(
-		(value: string | undefined) => updateRule(index, { value }),
-		[updateRule, index]
+		( value: string | undefined ) => updateRule( index, { value } ),
+		[ updateRule, index ]
 	);
 
 	/**
@@ -92,63 +94,63 @@ export const ConditionalLogicRule: React.FC<ConditionalLogicRuleProps> = (
 	 * @param condition The condition to update.
 	 */
 	const updateCondition = useCallback(
-		(condition: string | undefined) => updateRule(index, { condition }),
-		[updateRule, index]
+		( condition: string | undefined ) => updateRule( index, { condition } ),
+		[ updateRule, index ]
 	);
 
 	/**
 	 * Removes the rule.
 	 */
 	const localRemoveRule = useCallback(
-		() => removeRule(index),
-		[removeRule, index]
+		() => removeRule( index ),
+		[ removeRule, index ]
 	);
 
 	/**
 	 * The full rule.
 	 */
-	const ruleFull: string = rule.full ?? (rule.type ? `[[${rule.type}]]` : '');
+	const ruleFull: string = rule.full ?? ( rule.type ? `[[${ rule.type }]]` : '' );
 
 	/**
 	 * The closing bracket index of the rule.
 	 */
-	const closingBracketIndex = ruleFull.indexOf(']]');
+	const closingBracketIndex = ruleFull.indexOf( ']]' );
 
 	/**
 	 * The opening bracket index of the rule.
 	 */
-	const openingBracketIndex = ruleFull.indexOf('[[', closingBracketIndex);
+	const openingBracketIndex = ruleFull.indexOf( '[[', closingBracketIndex );
 
 	/**
 	 * Whether the rule has multiple tags.
 	 */
 	const hasMultipleTags =
 		closingBracketIndex === -1 ||
-		(openingBracketIndex !== -1 &&
-			openingBracketIndex > closingBracketIndex);
+		( openingBracketIndex !== -1 &&
+			openingBracketIndex > closingBracketIndex );
 
 	// Handles update of the full rule.
 	const onUpdateFull = useCallback(
-		(fullMergeTag: string | undefined) => {
+		( fullMergeTag: string | undefined ) => {
 			const toUpdate: Partial<ConditionalLogicRule> = {
 				full: fullMergeTag,
 			};
 
 			// Get first part between [[]].
-			if (fullMergeTag) {
+			if ( fullMergeTag ) {
 				const firstTag =
-					fullMergeTag.match(/\[\[([^\s\]]+)/)?.[1] || '';
+					fullMergeTag.match( /\[\[([^\s\]]+)/ )?.[ 1 ] || '';
 
-				if (firstTag) {
+				if ( firstTag ) {
 					toUpdate.type = firstTag
-						.replace('[[', '')
-						.replace(']]', '');
+						.replace( '[[', '' )
+						.replace( ']]', '' );
 				}
 			}
 
-			updateRule(index, toUpdate);
+			updateRule( index, toUpdate );
 		},
-		[updateRule, index]
+		[ updateRule, index ]
 	);
 
 	/**
@@ -157,86 +159,86 @@ export const ConditionalLogicRule: React.FC<ConditionalLogicRuleProps> = (
 	 * @param mergeTag The merge tag to click.
 	 */
 	const onMergeTagClick = useCallback(
-		(mergeTag: string) => {
+		( mergeTag: string ) => {
 			onUpdateFull(
-				ruleFull ? `${ruleFull} ${mergeTag}`.trim() : mergeTag
+				ruleFull ? `${ ruleFull } ${ mergeTag }`.trim() : mergeTag
 			);
 		},
-		[onUpdateFull, ruleFull]
+		[ onUpdateFull, ruleFull ]
 	);
 
 	/**
 	 * The merge tag suffix.
 	 */
-	const mergeTagSuffix = useMergeTags({
+	const mergeTagSuffix = useMergeTags( {
 		availableSmartTags: mergeTagsArray,
 		onMergeTagClick,
-	});
+	} );
 
 	// Value merge tag.
 	const onValueMergeTagClick = useCallback(
-		(mergeTag: string) => {
+		( mergeTag: string ) => {
 			updateValue(
-				rule.value ? `${rule.value} ${mergeTag}`.trim() : mergeTag
+				rule.value ? `${ rule.value } ${ mergeTag }`.trim() : mergeTag
 			);
 		},
-		[updateValue, rule.value]
+		[ updateValue, rule.value ]
 	);
 
 	/**
 	 * The value merge tag suffix.
 	 */
-	const valueMergeTagSuffix = useMergeTags({
+	const valueMergeTagSuffix = useMergeTags( {
 		availableSmartTags: mergeTagsArray,
 		onMergeTagClick: onValueMergeTagClick,
-	});
+	} );
 
 	/**
 	 * The smart tag.
 	 */
-	const smartTag = useMemo(() => {
+	const smartTag = useMemo( () => {
 		const tag = rule.type;
 
-		if (!tag) {
+		if ( !tag ) {
 			return null;
 		}
 
-		if (availableSmartTags[tag] !== undefined) {
-			return availableSmartTags[tag];
+		if ( availableSmartTags[ tag ] !== undefined ) {
+			return availableSmartTags[ tag ];
 		}
 
 		// Convert first occurrence of _ to .
-		const altTag = tag.replace('_', '.');
-		if (availableSmartTags[altTag] !== undefined) {
-			return availableSmartTags[altTag];
+		const altTag = tag.replace( '_', '.' );
+		if ( availableSmartTags[ altTag ] !== undefined ) {
+			return availableSmartTags[ altTag ];
 		}
 
-		for (const [key, value] of Object.entries(availableSmartTags)) {
+		for ( const [ key, value ] of Object.entries( availableSmartTags ) ) {
 			// Check without prefix.
-			if (key.indexOf('.') !== -1) {
-				const withoutPrefix = key.split('.').slice(1);
-				if (withoutPrefix.join('.') === tag) {
+			if ( key.indexOf( '.' ) !== -1 ) {
+				const withoutPrefix = key.split( '.' ).slice( 1 );
+				if ( withoutPrefix.join( '.' ) === tag ) {
 					return value;
 				}
 			}
 
 			// Converts a space or comma separated list to array.
-			const split = (string: string | string[]) =>
-				Array.isArray(string) ? string : string.split(/[\s,]+/);
+			const split = ( string: string | string[] ) =>
+				Array.isArray( string ) ? string : string.split( /[\s,]+/ );
 
 			// Check deprecated alternatives.
-			if (value.deprecated && split(value.deprecated).includes(tag)) {
+			if ( value.deprecated && split( value.deprecated ).includes( tag ) ) {
 				return value;
 			}
 		}
 
 		return null;
-	}, [rule.type, availableSmartTags]);
+	}, [ rule.type, availableSmartTags ] );
 
 	// Contains available options.
 	const availableOptions = usePlaceholder(
-		useOptions(smartTag?.options || []),
-		__('Select a value', 'newsletter-optin-box')
+		useOptions( smartTag?.options || [] ),
+		__( 'Select a value', 'newsletter-optin-box' )
 	);
 
 	// Checks whether the selected condition type has options.
@@ -249,41 +251,27 @@ export const ConditionalLogicRule: React.FC<ConditionalLogicRuleProps> = (
 
 	// Sets available comparisons for the selected condition.
 	const availableComparisons = usePlaceholder(
-		useMemo(() => {
+		useMemo( () => {
 			const types = [] as any[];
 
 			// Filter object of available condition types to include where key === rule.type.
-			Object.keys(comparisons).forEach((key) => {
-				const comparison_type = comparisons[key].type;
+			Object.keys( comparisons ).forEach( ( key ) => {
+				const comparison_type = comparisons[ key ].type;
 
-				if (hasOptions) {
-					if (
-						'string' === dataType &&
-						'is' != key &&
-						'is_not' != key
-					) {
-						return;
-					}
-
-					if (
-						'is_empty' === key ||
-						'is_not_empty' === key ||
-						'is_between' === key
-					) {
-						return;
-					}
+				if ( hasOptions && NON_OPTION_CONDITIONS.includes( key ) ) {
+					return;
 				}
 
-				if ('any' === comparison_type || comparison_type == dataType) {
-					types.push({
-						label: comparisons[key].name,
+				if ( 'any' === comparison_type || comparison_type == dataType ) {
+					types.push( {
+						label: comparisons[ key ].name,
 						value: key,
-					});
+					} );
 				}
-			});
+			} );
 			return types;
-		}, [dataType, comparisons]),
-		__('Select a comparison', 'newsletter-optin-box')
+		}, [ dataType, comparisons ] ),
+		__( 'Select a comparison', 'newsletter-optin-box' )
 	);
 
 	const skipValue =
@@ -291,70 +279,70 @@ export const ConditionalLogicRule: React.FC<ConditionalLogicRuleProps> = (
 
 	return (
 		<HStack justify="flex-start" wrap expanded>
-			<div style={{ minWidth: 320 }}>
+			<div style={ { minWidth: 320 } }>
 				<InputControl
 					type="text"
-					label={__('Smart Tag', 'newsletter-optin-box')}
-					hideLabelFromVision={true}
-					placeholder={__(
+					label={ __( 'Smart Tag', 'newsletter-optin-box' ) }
+					hideLabelFromVision={ true }
+					placeholder={ __(
 						'Enter a smart tag',
 						'newsletter-optin-box'
-					)}
-					value={ruleFull}
-					onChange={onUpdateFull}
+					) }
+					value={ ruleFull }
+					onChange={ onUpdateFull }
 					autoComplete="off"
-					suffix={mergeTagSuffix}
+					suffix={ mergeTagSuffix }
 					__next40pxDefaultSize
 				/>
 			</div>
-			<div style={{ width: 150 }}>
+			<div style={ { width: 150 } }>
 				<SelectControl
-					label={__('Comparison', 'newsletter-optin-box')}
-					hideLabelFromVision={true}
-					value={rule.condition ? rule.condition : 'is'}
-					options={availableComparisons}
-					onChange={updateCondition}
+					label={ __( 'Comparison', 'newsletter-optin-box' ) }
+					hideLabelFromVision={ true }
+					value={ rule.condition ? rule.condition : 'is' }
+					options={ availableComparisons }
+					onChange={ updateCondition }
 					size="default"
 					__next40pxDefaultSize
 					__nextHasNoMarginBottom
 				/>
 			</div>
 
-			{!skipValue && (
-				<div style={{ minWidth: 320 }}>
-					{hasOptions && (
+			{ !skipValue && (
+				<div style={ { minWidth: 320 } }>
+					{ hasOptions && (
 						<SelectControl
-							label={__('Value', 'newsletter-optin-box')}
-							hideLabelFromVision={true}
-							value={rule.value ? rule.value : ''}
-							options={availableOptions}
-							onChange={updateValue}
+							label={ __( 'Value', 'newsletter-optin-box' ) }
+							hideLabelFromVision={ true }
+							value={ rule.value ? rule.value : '' }
+							options={ availableOptions }
+							onChange={ updateValue }
 							size="default"
 							__next40pxDefaultSize
 							__nextHasNoMarginBottom
 						/>
-					)}
+					) }
 
-					{!hasOptions && (
+					{ !hasOptions && (
 						<InputControl
-							type={'number' === dataType ? 'number' : 'text'}
-							label={__('Value', 'newsletter-optin-box')}
-							placeholder={__(
+							type={ 'number' === dataType ? 'number' : 'text' }
+							label={ __( 'Value', 'newsletter-optin-box' ) }
+							placeholder={ __(
 								'Enter a value',
 								'newsletter-optin-box'
-							)}
-							hideLabelFromVision={true}
-							value={rule.value ? rule.value : ''}
-							onChange={updateValue}
-							suffix={valueMergeTagSuffix}
+							) }
+							hideLabelFromVision={ true }
+							value={ rule.value ? rule.value : '' }
+							onChange={ updateValue }
+							suffix={ valueMergeTagSuffix }
 							__next40pxDefaultSize
 						/>
-					)}
+					) }
 				</div>
-			)}
+			) }
 
 			<Button
-				onClick={localRemoveRule}
+				onClick={ localRemoveRule }
 				icon="trash"
 				variant="tertiary"
 				isDestructive
