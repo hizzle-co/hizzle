1f7ca541837fa5538149b3c34c30a74e7077ae1d
picocodes <picocodes@gmail.com>
2026-01-09 07:43:08 +0300

    build

--- src/build/records/index.asset.php
+++ src/build/records/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('hizzlewp-components', 'react', 'react-jsx-runtime', 'wp-components', 'wp-compose', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => 'd1699c0c064839b4ebe9');
+<?php return array('dependencies' => array('hizzlewp-components', 'react', 'react-jsx-runtime', 'wp-components', 'wp-compose', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => '65ce96012cb484783e39');
