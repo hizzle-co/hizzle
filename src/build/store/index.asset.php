995f2c102965600780fa6a34e165ee3305e9337d
picocodes <picocodes@gmail.com>
2026-03-06 09:22:38 +0300

    update packages and build

--- src/build/store/index.asset.php
+++ src/build/store/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-api-fetch', 'wp-compose', 'wp-data', 'wp-element', 'wp-url'), 'version' => '5a22f7990c43bf43e24c');
+<?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-api-fetch', 'wp-compose', 'wp-data', 'wp-element', 'wp-url'), 'version' => '03cbd85c55cdb5667d8a');
