c2965da1cf80042aa1f935032d41e7b7eeffd944
picocodes <picocodes@gmail.com>
2026-05-28 15:18:43 +0300

    Interface improvements

--- packages/interface/src/interface.tsx
+++ packages/interface/src/interface.tsx
@@ -13,6 +13,12 @@ import {
 import { store as preferencesStore } from '@wordpress/preferences';
 import { useSelect, useDispatch } from '@wordpress/data';
 import { useViewportMatch } from '@wordpress/compose';
+import { __experimentalVStack as VStack } from '@wordpress/components';
+
+/**
+ * Local dependencies.
+ */
+import { InterfaceNotices, InterfaceSnackbars } from './notices';
 
 const STORE_NAME = 'hizzlewp/interface';
 
@@ -50,6 +56,61 @@ const WithFullscreenMode = ( { children, storeName = STORE_NAME }: { children: R
 };
 
 /**
+ * Props for the main interface component for the application.
+ */
+export interface InterfaceProps extends Record<string, any> {
+	/**
+	 * Header component.
+	 */
+	header?: React.ReactNode;
+
+	/**
+	 * Sidebar component.
+	 */
+	sidebar?: React.ReactNode;
+
+	/**
+	 * Secondary sidebar component.
+	 */
+	secondarySidebar?: React.ReactNode;
+
+	/**
+	 * Content component.
+	 */
+	content?: React.ReactNode;
+
+	/**
+	 * Footer component.
+	 */
+	footer?: React.ReactNode;
+
+	/**
+	 * Action components.
+	 */
+	actions?: React.ReactNode;
+
+	/**
+	 * Editor notices component.
+	 */
+	editorNotices?: React.ReactNode | true;
+
+	/**
+	 * Whether the interface is in distraction-free mode.
+	 */
+	isDistractionFree?: boolean;
+
+	/**
+	 * Interface labels.
+	 */
+	labels?: Record<string, string>;
+
+	/**
+	 * Additional CSS class names.
+	 */
+	className?: string;
+}
+
+/**
  * The main interface component for the application.
  *
  * @param {Object} props                     Component props.
@@ -59,22 +120,36 @@ const WithFullscreenMode = ( { children, storeName = STORE_NAME }: { children: R
  * @param {React.ReactNode} [props.content]  Content component.
  * @param {React.ReactNode} [props.footer]   Footer component.
  * @param {React.ReactNode} [props.actions]  Action components.
- * @param {React.ReactNode} [props.editorNotices] Editor notices component.
+ * @param {React.ReactNode|true} [props.editorNotices] Editor notices component.
  * @param {boolean} [props.isDistractionFree] Whether the interface is in distraction free mode.
  * @param {Object} [props.labels]            Interface labels.
  * @param {string} [props.className]         Additional CSS class names.
  * @return {JSX.Element}              The interface component.
  */
-export const Interface = ( { className = '', storeName = STORE_NAME, ...rest }: { className?: string, storeName?: string } & Record<string, any> ) => {
+export const Interface = ( { className = '', storeName = STORE_NAME, content, editorNotices, ...rest }: InterfaceProps ) => {
 	const useClassName = [ 'hizzlewp-app__interface', className ]
 		.filter( Boolean )
 		.join( ' ' );
+
+	let newContent = content;
+
+	if ( true === editorNotices ) {
+		newContent = (
+			<VStack>
+				<InterfaceSnackbars />
+				<InterfaceNotices />
+				{ content }
+			</VStack>
+		);
+	}
 
 	return (
 		<WithFullscreenMode storeName={ storeName }>
 			<InterfaceSkeleton
 				className={ useClassName }
 				isDistractionFree={ false }
+				content={ newContent }
+				editorNotices={ true === editorNotices ? undefined : editorNotices }
 				{ ...rest }
 			/>
 		</WithFullscreenMode>
