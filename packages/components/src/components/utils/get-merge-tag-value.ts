ede09323e0fa0c84b972ccd96f703eb72ab1b092
picocodes <picocodes@gmail.com>
2026-08-14 07:15:33 +0300

    chore: Add attributesToMergeTag util

--- packages/components/src/components/utils/get-merge-tag-value.ts
+++ packages/components/src/components/utils/get-merge-tag-value.ts
@@ -9,14 +9,45 @@ import type { smartTag } from '../hooks';
  * @param {smartTag} smartTag - The smart tag to get the value of.
  * @returns {string} The value of the smart tag.
  */
-export const getMergeTagValue = (smartTag: smartTag): string => {
-	if (smartTag.example) {
+export const getMergeTagValue = ( smartTag: smartTag ): string => {
+	if ( smartTag.example ) {
 		return smartTag.example;
 	}
 
-	if (!smartTag.default) {
-		return `${smartTag.smart_tag}`;
+	if ( !smartTag.default ) {
+		return `${ smartTag.smart_tag }`;
 	}
 
-	return `${smartTag.smart_tag} default="${smartTag.default}"`;
+	return `${ smartTag.smart_tag } default="${ smartTag.default }"`;
+};
+
+/**
+ * Converts merge tag attributes into a complete merge tag.
+ */
+export const attributesToMergeTag = (
+	attributes: Record<string, unknown>,
+	mergeTag: string
+): string => {
+	let attributesString = '';
+
+	if ( attributes ) {
+		Object.entries( attributes || {} ).forEach( ( [ key, value ] ) => {
+			if ( value === '' || value === null || value === undefined ) {
+				return;
+			}
+
+			// Convert boolean values to 1 or 0.
+			if ( typeof value === 'boolean' ) {
+				value = value ? 1 : 0;
+			}
+
+			// Do not add quotes to numeric values.
+			attributesString +=
+				typeof value === 'number'
+					? ` ${ key }=${ value }`
+					: ` ${ key }="${ String( value ).replaceAll( '"', '&quot;' ) }"`;
+		} );
+	}
+
+	return `[[${ mergeTag }${ attributesString }]]`;
 };
