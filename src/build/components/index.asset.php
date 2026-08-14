6ca3b1e746f5df9486f1ab49c29a0482e461ef87
picocodes <picocodes@gmail.com>
2026-08-14 08:24:56 +0300

    build

--- src/build/components/index.asset.php
+++ src/build/components/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-api-fetch', 'wp-components', 'wp-compose', 'wp-date', 'wp-element', 'wp-i18n', 'wp-keycodes', 'wp-primitives', 'wp-url'), 'version' => '320e3b3f912beeb9c777');
+<?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-api-fetch', 'wp-components', 'wp-compose', 'wp-date', 'wp-element', 'wp-i18n', 'wp-keycodes', 'wp-primitives', 'wp-url'), 'version' => 'c24a079855132b9b3c69');
